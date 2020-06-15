193a9c222bb75490b959375c14d379aefbfeb210
liyunzhi1993 <liyunzhi1993@gmail.com>
2020-06-15 17:04:54 +0800

    fix

--- Json.js
+++ Json.js
@@ -3,9 +3,9 @@ const userInfo = {
 	status: 1,
 	data: {
 		id: 1,
-		mobile: 18888888888,
-		nickname: 'Leo yo',
-		portrait: 'http://img.61ef.cn/news/201409/28/2014092805595807.jpg'
+		mobile: 15002177881,
+		nickname: '李允智',
+		portrait: 'http://qbrbzkpmz.bkt.clouddn.com/user-avtar.jpg'
 	},
 	msg: '提示'
 }
---
+++ pages/invite/add.vue
@@ -0,0 +1,207 @@
+<template>
+	<view class="content">
+		<view class="">
+			<view class="user-section">
+				<image class="bg" src="http://qbrbzkpmz.bkt.clouddn.com/user-bg.jpg"></image>
+				<view class="user-info-box">
+					<view class="portrait-box">
+						<!-- <image class="portrait" :src="userInfo.portrait || '/static/missing-face.png'"></image> -->
+						<image class="portrait" src="http://qbrbzkpmz.bkt.clouddn.com/user-avtar.jpg"></image>
+					</view>
+					<view class="info-box">
+						<!-- <text class="username">{{userInfo.nickname || '游客'}}</text> -->
+						<text class="username">倪同学邀请你来赚钱啦</text>
+					</view>
+				</view>
+				<!-- <view class="vip-card-box">
+					<image class="card-bg" src="/static/vip-card-bg.png" mode=""></image>
+					<view class="b-btn">
+						立即开通
+					</view>
+					<view class="tit">
+						<text class="yticon icon-iLinkapp-"></text>
+						DCloud会员
+					</view>
+					<text class="e-m">DCloud Union</text>
+					<text class="e-b">开通会员开发无bug 一测就上线</text>
+				</view> -->
+			</view>
+			</view>
+		<view class="row b-b">
+			<text class="tit">姓名</text>
+			<input class="input" type="text" v-model="addressData.name" placeholder="您的姓名" placeholder-class="placeholder" />
+		</view>
+		<view class="row b-b">
+			<text class="tit">手机号</text>
+			<input class="input" type="number" v-model="addressData.mobile" placeholder="您的手机号" placeholder-class="placeholder" />
+		</view>
+		<view class="row b-b">
+			<text class="tit">地址</text>
+			<text @click="chooseLocation" class="input">
+				{{addressData.addressName}}
+			</text>
+			<text class="yticon icon-shouhuodizhi"></text>
+		</view>
+		<button class="add-btn" @click="confirm">提交</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				addressData: {
+					name: '',
+					mobile: '',
+					addressName: '在地图选择',
+					address: '',
+					area: '',
+					default: false
+				}
+			}
+		},
+		onLoad(option){
+			let title = '邀请你啦！';
+			if(option.type==='edit'){
+				title = '编辑收货地址'
+				
+				this.addressData = JSON.parse(option.data)
+			}
+			this.manageType = option.type;
+			uni.setNavigationBarTitle({
+				title
+			})
+		},
+		methods: {
+			switchChange(e){
+				this.addressData.default = e.detail;
+			},
+			
+			//地图选择地址
+			chooseLocation(){
+				uni.chooseLocation({
+					success: (data)=> {
+						this.addressData.addressName = data.name;
+						this.addressData.address = data.name;
+					}
+				})
+			},
+			
+			//提交
+			confirm(){
+				let data = this.addressData;
+				if(!data.name){
+					this.$api.msg('请填写您的姓名');
+					return;
+				}
+				if(!/(^1[3|4|5|7|8][0-9]{9}$)/.test(data.mobile)){
+					this.$api.msg('请输入正确的手机号码');
+					return;
+				}
+				if(!data.address){
+					this.$api.msg('请在地图选择所在位置');
+					return;
+				}
+				// if(!data.area){
+				// 	this.$api.msg('请填写门牌号信息');
+				// 	return;
+				// }
+				
+				//this.$api.prePage()获取上一页实例，可直接调用上页所有数据和方法，在App.vue定义
+				this.$api.prePage().refreshList(data, this.manageType);
+				this.$api.msg(`邀请信息${this.manageType=='edit' ? '修改': '填写'}成功`);
+				setTimeout(()=>{
+					uni.navigateBack()
+				}, 800)
+			},
+		}
+	}
+</script>
+
+<style lang="scss">
+	page{
+		background: $page-color-base;
+		padding-top: 16upx;
+	}
+
+	.row{
+		display: flex;
+		align-items: center;
+		position: relative;
+		padding:0 30upx;
+		height: 110upx;
+		background: #fff;
+		
+		.tit{
+			flex-shrink: 0;
+			width: 120upx;
+			font-size: 30upx;
+			color: $font-color-dark;
+		}
+		.input{
+			flex: 1;
+			font-size: 30upx;
+			color: $font-color-dark;
+		}
+		.icon-shouhuodizhi{
+			font-size: 36upx;
+			color: $font-color-light;
+		}
+	}
+	.default-row{
+		margin-top: 16upx;
+		.tit{
+			flex: 1;
+		}
+		switch{
+			transform: translateX(16upx) scale(.9);
+		}
+	}
+	.add-btn{
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 690upx;
+		height: 80upx;
+		margin: 60upx auto;
+		font-size: $font-lg;
+		color: #fff;
+		background-color: $base-color;
+		border-radius: 10upx;
+		box-shadow: 1px 2px 5px rgba(219, 63, 96, 0.4);
+	}
+	.user-section{
+		height: 520upx;
+		padding: 100upx 30upx 0;
+		position:relative;
+		.bg{
+			position:absolute;
+			left: 0;
+			top: 0;
+			width: 100%;
+			height: 100%;
+			filter: blur(1px);
+			opacity: .7;
+		}
+	}
+	.user-info-box{
+		height: 180upx;
+		display:flex;
+		align-items:center;
+		position:relative;
+		z-index: 1;
+		.portrait{
+			width: 130upx;
+			height: 130upx;
+			border:5upx solid #fff;
+			border-radius: 50%;
+		}
+		.username{
+			font-size: $font-lg + 6upx;
+			color: $font-color-dark;
+			margin-left: 20upx;
+		}
+	}
+	
+	
+</style>
